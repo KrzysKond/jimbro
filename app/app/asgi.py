bda20f425d5db096c43993f6f073d64abb172c35
KrzysKond <gaxkk19@gmail.com>
2024-10-16 01:01:39 +0200

    corrected asgi.py

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -12,13 +12,16 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+django_asgi_app = get_asgi_application()
+
 from .middleware import TokenAuthMiddleware
 from groupchat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AllowedHostsOriginValidator(
         TokenAuthMiddleware(
             URLRouter(
--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,4 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-daphne -b 0.0.0.0 -p 9000 app.asgi:application &
-
-python manage.py runworker
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
