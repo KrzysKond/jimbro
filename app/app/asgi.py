9bccb21e4cdd5486b18b2bd7fdf3b7dcc6ee5db3
KrzysKond <gaxkk19@gmail.com>
2024-10-22 12:11:59 +0200

    fixed nginx configuration for asgii

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -8,14 +8,18 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from .middleware import TokenAuthMiddleware
-from groupchat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+
+
+from groupchat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
